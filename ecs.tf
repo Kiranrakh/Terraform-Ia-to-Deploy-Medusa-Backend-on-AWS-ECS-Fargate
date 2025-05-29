76c9b5870910626cb88b59ff1b64534ea35e647c
Kiran Barikrao Rakh <124512771+Kiranrakh@users.noreply.github.com>
2025-05-29 09:06:38 +0530

    ecs.tf

---
+++ ecs.tf
@@ -0,0 +1,27 @@
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  container_definitions    = file("${path.module}/ecs_task_definition.json")
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_subnet.id]
+    assign_public_ip = true
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+}
