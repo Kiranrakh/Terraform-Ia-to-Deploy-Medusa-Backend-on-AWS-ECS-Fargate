e57b5e3fe446aa7abe26db0f42edf66cdb3b91c1
Kiran Barikrao Rakh <124512771+Kiranrakh@users.noreply.github.com>
2025-05-29 09:05:02 +0530

    public-rt.tf

---
+++ public-rt.tf
@@ -0,0 +1,14 @@
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
